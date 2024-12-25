90359e76a660c9d6f24b004d19319b6112ed9bc5
Valarmorguliz <156031365+Valarmorguliz@users.noreply.github.com>
2024-12-25 12:29:25 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,6 +7,32 @@ document.addEventListener("DOMContentLoaded", function () {
     images.forEach((img) => {
         img.style.pointerEvents = "none";
     });
+
+    // Скрипт для обработки отправки формы
+    document.getElementById('application-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const formData = new FormData(this);
+
+        fetch('https://formspree.io/f/mnnqyedw', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = 'page8.html'; // замените на URL следующей страницы
+            } else {
+                alert('Произошла ошибка при отправке формы. Пожалуйста, попробуйте ещё раз.');
+            }
+        })
+        .catch(error => {
+            console.error('Ошибка:', error);
+            alert('Произошла ошибка при отправке формы. Пожалуйста, попробуйте ещё раз.');
+        });
+    });
 });
 
 // Отключение выделения текста через JavaScript
@@ -21,7 +47,7 @@ document.addEventListener("dragstart", function (e) {
     }
 });
 
-// Скрипт для отключения правого клика 
+// Скрипт для отключения правого клика
 document.addEventListener('contextmenu', function (e) {
-      e.preventDefault();
-    });
+    e.preventDefault();
+});
